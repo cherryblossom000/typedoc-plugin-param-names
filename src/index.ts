1e1ccba6885b9704fc2d409b54aeeb7ae9436019
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-07-06 17:15:34 +1000

    feat: initial release

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {ParameterType} from 'typedoc'
+import {Converter} from 'typedoc/dist/lib/converter'
+import type {ParameterReflection} from 'typedoc'
+import type {Context} from 'typedoc/dist/lib/converter'
+import type {PluginHost} from 'typedoc/dist/lib/utils'
+
+export const load = ({application}: PluginHost): void => {
+  application.options.addDeclaration({
+    name: 'param-name',
+    help: 'Specifies the name to replace __namedParameters with.',
+    type: ParameterType.String,
+    defaultValue: 'options'
+  })
+
+  application.converter.on(Converter.EVENT_CREATE_PARAMETER, (_: Context, param: ParameterReflection) => {
+    if (param.name === '__namedParameters') param.name = application.options.getValue('param-name') as string
+  })
+}
