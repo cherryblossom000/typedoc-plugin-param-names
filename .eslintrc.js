dba11b90d9f43f13e9f76654a950b0ac76944b72
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-01-12 15:55:26 +1100

    chore: fix linting

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/** @type {import('eslint').Linter.Config & {parserOptions?: import('@typescript-eslint/parser').ParserOptions}} */
+const config = {
+  root: true,
+  extends: ['@cherryblossom/eslint-config/node'],
+  reportUnusedDisableDirectives: true,
+  parserOptions: {
+    project: 'src/tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  settings: {jsdoc: {mode: 'typescript'}},
+  ignorePatterns: ['dist/', 'test/', '.history/'],
+  rules: {
+    '@typescript-eslint/prefer-readonly-parameter-types': 0,
+    'import/no-unused-modules': 0,
+    'import/prefer-default-export': 0
+  }
+}
+module.exports = config
